a54f0abcc86876f5a4f014947a92a00c050589f7
Niki_Tobosharov <niki6325@abv.bg>
2024-12-09 15:00:13 +0200

    implemented the mobile nav

---
+++ script.js
@@ -0,0 +1,58 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
+    if (
+      href.startsWith("https://") ||
+      href.startsWith("tel") ||
+      href.startsWith("mailto")
+    )
+      return;
+
+    e.preventDefault();
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+const sectionHeroEl = document.querySelector(".header");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
